d6781e5eb24182def75c1057d1e1eaca570aa401
LydianLights <rane.fields@gmail.com>
2023-07-17 02:08:08 -0400

    Add tailwind and base content

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+import colors from "tailwindcss/colors";
+import plugin from "tailwindcss/plugin";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{html,js,svelte,ts}"],
+    darkMode: "class",
+    theme: {
+        extend: {},
+        colors: {
+            transparent: "transparent",
+            current: "currentColor",
+            black: "#222222",
+            white: "#ffffff",
+            grey: {
+                ...colors.zinc,
+            },
+            primary: {
+                light: colors.violet[500],
+                DEFAULT: colors.violet[600],
+                dark: colors.violet[700],
+            },
+        },
+    },
+    plugins: [
+        plugin(function ({ addVariant }) {
+            addVariant("hocus", ["&:hover", "&:focus"]);
+            addVariant("hocus-within", ["&:hover", "&:focus-within"]);
+            addVariant("hocus-visible", ["&:hover", "&:focus-visible"]);
+        }),
+    ],
+};
